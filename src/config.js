b14930559cb9c505b2e3294d504f69b807ab903b
Jordan Leach <jordonleach@gmail.com>
2019-01-07 11:05:38 -0500

    added login/signup/signout with cognito

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "thread-orders-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://x8h56wj5gk.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_IY63fXHtd",
+    APP_CLIENT_ID: "qskkjg2g281ecjj9glmqhnu77",
+    IDENTITY_POOL_ID: "us-east-1:ebfb24c0-62e2-4f1c-9956-c44ab3e1851d"
+  }
+};
